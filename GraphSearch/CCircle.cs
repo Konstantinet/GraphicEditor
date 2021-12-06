3b5243e155c862e9f0abd24de665bb9e8744d77b
Konstantinet <kostya222lb@mail.ru>
2021-12-06 17:54:05 +0500

    Basic circle creation

---
+++ GraphSearch/CCircle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace GraphSearch
+{
+    public class CCircle
+    {
+        public double X, Y;
+        private int Radius = 50;
+        public bool Selected { get; private set; }
+
+        public CCircle(double x, double y)
+        {
+            X = x;
+            Y = y;
+            Selected = true;
+        } 
+        public bool HitTest(double x,double y)
+        {
+            if ((Math.Pow((x-X),2) + Math.Pow((y-Y),2)) <= Radius)
+                return true;
+            else
+                return false;
+        }
+        public void Paint(Canvas canvas)
+        {
+            //Clear canvas
+            var e = new Ellipse();
+            if (Selected)
+                e.Stroke = Brushes.Red;
+            else
+                e.Stroke = Brushes.Black;
+            e.Width = Radius;
+            e.Height = Radius;
+            Canvas.SetLeft(e, X-(Radius/2));
+            Canvas.SetTop(e, Y-(Radius/2));
+            //Canvas.SetZIndex(e,2)
+            canvas.Children.Add(e);
+
+        }
+    
+        public void Select() { Selected = true; }
+        public void Unselect() { Selected = false;  }
+
+    }
+}
---
+++ GraphSearch/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace GraphSearch
+{
+    public partial class MainWindow : Window
+    {
+        public List<CCircle> circles;
+        public MainWindow()
+        {
+            InitializeComponent();
+            circles = new List<CCircle>();
+            
+        }
+
+        //protected override void OnRender(DrawingContext drawingContext)
+        //{
+        //    base.OnRender(drawingContext);
+        //    foreach (var c in circles)
+        //    {
+        //        //drawingContext.DrawEllipse(Brushes.White, new Pen(Brushes.Black, 3), new Point(c.X, c.Y), 50, 50);
+        //        c.Paint(canvas);
+
+        //    }
+        //}
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+           
+            
+            foreach(var circle in circles)
+            {
+                if (circle.HitTest(e.GetPosition(this).X, e.GetPosition(this).Y)){
+                    if(!(Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl)))
+                    {
+                        UncheckAll();
+                    }
+                    circle.Select();
+                    return;
+                }
+            }
+
+            
+            Point pt = e.GetPosition(this);
+            UncheckAll();
+            var Vertex = new CCircle(e.GetPosition(this).X, e.GetPosition(this).Y);
+            circles.Add(Vertex);
+            Vertex.Paint(canvas);
+            
+        }
+
+       
+        void UncheckAll()
+        {
+            foreach(var c in circles)
+            {
+                c.Unselect();
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Delete)
+            {
+                foreach(var c in circles)
+                {
+                    if (c.Selected == true)
+                    {
+                        circles.Remove(c);
+                    }
+                }
+            }
+        }
+
+    }
+}
