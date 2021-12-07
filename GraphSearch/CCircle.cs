91a402e681995acd77051797ae8ba25425096c03
Konstantinet <kostya222lb@mail.ru>
2021-12-07 16:45:22 +0500

    Shapes selection added

--- GraphSearch/CCircle.cs
+++ GraphSearch/CCircle.cs
@@ -12,38 +12,50 @@ namespace GraphSearch
         private int Radius = 50;
         public bool Selected { get; private set; }
 
+        public Ellipse e;
+
         public CCircle(double x, double y)
         {
             X = x;
             Y = y;
             Selected = true;
-        } 
+        }
         public bool HitTest(double x,double y)
         {
-            if ((Math.Pow((x-X),2) + Math.Pow((y-Y),2)) <= Radius)
+            if ((Math.Pow((x-X),2) + Math.Pow((y-Y),2)) <= Math.Pow(Radius,2))
                 return true;
             else
                 return false;
         }
         public void Paint(Canvas canvas)
         {
-            //Clear canvas
-            var e = new Ellipse();
+            if (e == null)
+            {
+                e = new Ellipse();
+                e.Width = Radius;
+                e.Height = Radius;
+                e.Fill = Brushes.White;
+                Canvas.SetLeft(e, X - (Radius / 2));
+                Canvas.SetTop(e, Y - (Radius / 2));
+                //Canvas.SetZIndex(e,2)
+                canvas.Children.Add(e);
+            }
             if (Selected)
                 e.Stroke = Brushes.Red;
             else
-                e.Stroke = Brushes.Black;
-            e.Width = Radius;
-            e.Height = Radius;
-            Canvas.SetLeft(e, X-(Radius/2));
-            Canvas.SetTop(e, Y-(Radius/2));
-            //Canvas.SetZIndex(e,2)
-            canvas.Children.Add(e);
-
+                e.Stroke = Brushes.Black;            
+            canvas.InvalidateVisual();
         }
-    
-        public void Select() { Selected = true; }
-        public void Unselect() { Selected = false;  }
 
+        public void Remove(Canvas canvas)
+        {
+            if (e != null)
+            {
+                canvas.Children.Remove(e);
+            }
+        }
+
+        public void Select() => Selected = true;
+        public void Unselect() => Selected = false;
     }
 }
--- GraphSearch/MainWindow.xaml.cs
+++ GraphSearch/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace GraphSearch
 {
@@ -15,40 +17,26 @@ namespace GraphSearch
             circles = new List<CCircle>();
             
         }
-
-        //protected override void OnRender(DrawingContext drawingContext)
-        //{
-        //    base.OnRender(drawingContext);
-        //    foreach (var c in circles)
-        //    {
-        //        //drawingContext.DrawEllipse(Brushes.White, new Pen(Brushes.Black, 3), new Point(c.X, c.Y), 50, 50);
-        //        c.Paint(canvas);
-
-        //    }
-        //}
         private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
-           
-            
             foreach(var circle in circles)
             {
                 if (circle.HitTest(e.GetPosition(this).X, e.GetPosition(this).Y)){
+                  
                     if(!(Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl)))
                     {
                         UncheckAll();
                     }
-                    circle.Select();
+                    SelectCircle(circle);
+                    e.Handled = true;
                     return;
                 }
             }
-
-            
             Point pt = e.GetPosition(this);
             UncheckAll();
             var Vertex = new CCircle(e.GetPosition(this).X, e.GetPosition(this).Y);
             circles.Add(Vertex);
-            Vertex.Paint(canvas);
-            
+            Vertex.Paint(canvas);  
         }
 
        
@@ -56,20 +44,37 @@ namespace GraphSearch
         {
             foreach(var c in circles)
             {
-                c.Unselect();
+                UnselectCircle(c);
             }
+        }
+        void SelectCircle(CCircle circle)
+        {
+            circle.Select();
+            circle.Paint(canvas);
+        }
+        void UnselectCircle(CCircle circle)
+        {
+            circle.Unselect();
+            circle.Paint(canvas);
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
             if(e.Key == Key.Delete)
             {
+                List<CCircle> deleted = new List<CCircle>();
                 foreach(var c in circles)
                 {
                     if (c.Selected == true)
                     {
-                        circles.Remove(c);
+                        deleted.Add(c);
                     }
+                }
+                foreach(var c in deleted)
+                {
+                    c.Remove(canvas);
+                    circles.Remove(c);
+                    
                 }
             }
         }
